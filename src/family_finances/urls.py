a9cec160bcc0f526e149907bcacf0cea444c1ad8
Denis Ivanov <dnb8866@gmail.com>
2024-11-18 14:56:03 +0700

    Добавление drf-yasg. Генерация динамической документации.

--- src/family_finances/urls.py
+++ src/family_finances/urls.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.v1.urls', namespace='api_v1'))
+]
+
+
+# Генерация динамической документации
+schema_view = get_schema_view(
+    info=openapi.Info(
+      title="Reference API",
+      default_version='v1',
+      description="RESTful API справочника"
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+urlpatterns += [
+   re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+       schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+       name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+       name='schema-redoc'),
 ]
