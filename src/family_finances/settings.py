3d3791f8f4b94103ed28eecba2a9863dfd698a73
Denis Ivanov <dnb8866@gmail.com>
2024-11-20 15:31:13 +0700

    Добавлен django-filter = "^24.3"

--- src/family_finances/settings.py
+++ src/family_finances/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_yasg',
+    'django_filters',
     'django_extensions',
     'users',
     'transactions',
@@ -119,3 +120,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
