e0c130f042e3819261f4743e4d7ad3d4402b680c
Denis Ivanov <dnb8866@gmail.com>
2024-11-20 19:04:33 +0700

    Добавлена проверка на наличие статьи перед добавлением транзакции. Добавлен сериализатор для Summary.

--- src/api/v1/transactions/serializers.py
+++ src/api/v1/transactions/serializers.py
@@ -1,25 +1,53 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from .validators import PeriodYearValidator, PeriodMonthValidator
 from family_finances import constants
 from transactions.models import Transaction, Summary, Basename
 
 
-class TransactionSerializer(serializers.ModelSerializer):
+class TransactionCreateSerializer(serializers.ModelSerializer):
     type_transaction = serializers.ChoiceField(
         choices=constants.CHOICE_TYPE_TRANSACTION
-    )
-    period_year = serializers.IntegerField(
-        validators=(PeriodYearValidator(),)
-    )
-    period_month = serializers.IntegerField(
-        validators=(PeriodMonthValidator(),)
     )
 
     class Meta:
         model = Transaction
-        fields = '__all__'
+        fields = (
+            'type_transaction',
+            'group_name',
+            'description',
+            'value_transaction',
+            'author'
+        )
         read_only_fields = ('author', 'basename')
+
+    def create(self, validated_data):
+        summary = Summary.objects.filter(
+            basename=validated_data['basename'],
+            period_month=validated_data['period_month'],
+            period_year=validated_data['period_year'],
+            type_transaction=validated_data['type_transaction'],
+            group_name=validated_data['group_name']
+        )
+        if not summary.exists():
+            raise ValidationError(
+                {
+                    'group_name': f'Статья {validated_data['group_name']} '
+                                  f'не найдена.'
+                }
+            )
+        instance = Transaction.objects.create(**validated_data)
+        return instance
+
+    def validate(self, attrs):
+        return attrs
+
+
+class TransactionDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Transaction
 
 
 class GroupCreateSerializer(serializers.ModelSerializer):
@@ -82,3 +110,17 @@ class GroupDetailSerializer(serializers.ModelSerializer):
         representation = super().to_representation(instance)
         representation['owner_base_username'] = instance.basename.user.username
         return representation
+
+
+class SummarySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'type_transaction',
+            'group_name',
+            'plan_value',
+            'fact_value',
+            'created_at',
+            'updated_at'
+        )
+        model = Summary
