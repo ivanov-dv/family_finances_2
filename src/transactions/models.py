6ef6760f141caae4376a20a339b54b1a096a371f
Denis Ivanov <dnb8866@gmail.com>
2024-11-20 15:30:22 +0700

    Корректировка модели Summary

--- src/transactions/models.py
+++ src/transactions/models.py
@@ -70,7 +70,11 @@ class Summary(CreatedUpdatedModel):
     )
     group_name = models.CharField(max_length=30)
     plan_value = models.DecimalField(max_digits=12, decimal_places=2)
-    fact_value = models.DecimalField(max_digits=12, decimal_places=2)
+    fact_value = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        default=0
+    )
 
     class Meta:
         default_related_name ='summaries'
