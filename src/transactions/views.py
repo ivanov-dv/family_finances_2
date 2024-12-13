212a6f9a4932763a47c976f4986bafbc9c18665f
Denis Ivanov <dnb8866@gmail.com>
2024-12-13 12:05:28 +0700

    test CI/CD

--- src/transactions/views.py
+++ src/transactions/views.py
@@ -8,7 +8,7 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'FF test'
+        context['title'] = 'FF'
         return context
 
 
