c7ee0a35ca29ef456169041840e8c88c3cba32ee
Denis Ivanov <dnb8866@gmail.com>
2024-12-13 15:05:02 +0700

    валидация логина при регистрации на зарезервированные имена

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.conf import settings
 
 from .models import User
 
@@ -11,10 +12,16 @@ class RegistrationForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ('username', 'password')
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
         if User.objects.filter(username=username).exists():
-            raise forms.ValidationError('Имя пользователя уже занято')
+            raise forms.ValidationError(
+                f'Логин {username} уже занят'
+            )
+        if username in settings.NOT_ALLOWED_USERNAMES:
+            raise forms.ValidationError(
+                f'Логин {username} зарезервирован'
+            )
         return username
