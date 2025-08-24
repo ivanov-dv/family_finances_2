abce3e64486676bbf8f0fcdc8ffafb42a5e7ef05
Denis Ivanov <dnb8866@gmail.com>
2025-08-24 13:01:14 +0700

    Добавлены докстринги в api.v1.transactions.

--- src/api/v1/transactions/validators.py
+++ src/api/v1/transactions/validators.py
@@ -2,6 +2,8 @@ from rest_framework.serializers import ValidationError
 
 
 class PeriodMonthValidator:
+    """Валидатор для проверки месяца в диапазоне от 1 до 12."""
+
     def __call__(self, period_month):
         if period_month not in range(1, 13):
             raise ValidationError(
@@ -10,6 +12,8 @@ class PeriodMonthValidator:
 
 
 class PeriodYearValidator:
+    """Валидатор для проверки года в диапазоне от 2024 до 2099."""
+
     def __call__(self, period_year):
         if period_year not in range(2024, 2100):
             raise ValidationError(
